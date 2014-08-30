dd14b35e3060ae3626e63228feed49189c36e34b
malukenho <admin@phpse.net>
2014-08-30 01:54:38 -0300

    Added Google App Engine slide

---
+++ slides/app_engine.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+clear
+CURRENT_DATETIME=$(date)
+echo -e "
+\e[1;36m
+                                                               .......                                              
+                                                             ...';c;'..                                             
+                                                             .'coddl;'..                                            
+                                                             .,oddddl;'...                                          
+                                                             .,odddool;,'..                                         
+                                                            ..,oooooooc;,'...           .......                     
+                                                            ..,loooolllc;,,... .......'',;:::;,'....                
+                                                            ..,llllllllcc;,,'...,:loxO0KKKKKKKKOkoc'..              
+                                                            ..,llllcccccc:;:cldOKXXXXXKKKKK00KKKKKKOo;..            
+                                                            ..,ccccccc:cldk0KKKXXXXKKKKkdlccclodk0KKKKx;..          
+                                                            ..,:::::coxO0KKKKKXXXXXK0kxoldolllooodxOKKK0l'.         
+                                                            ..,:::lx0KKKKKKKXXXXXXKOxk0KOxkkdoddxxxxk0KKKd'.        
+                                                            ..,:lk0KKKKKKKKKXXXXXKkxkO0KOkO0Odxxxxxxxk0KKKl..       
+                                                          ...;oO0KKKKKKKKKKXXXXXKkxxkOKKO0KK0xxxxxxxxxk0KK0:..      
+                                                         ..;d000K0KKKKKKKKKKKXXXklccclodOK0OkkkkkxxxxxxOKKKd..      
+                                                        ..;k000000KKKKKKKKKXXXXKdccccccclxOO0000xxxxxxxx0KKO,.      
+                                                        ..o0000000KKKKKKKKKXXNXKl::::cololk0000kdxxxxxxx0KKO;.      
+                                                        ..dK000KKKKKKKKKKKKKXXX0l::::cdloldkkkkkxxxxxxxx0KKO,.      
+                                                     .....:lllccccc::::::OKKKKKKo;;;;;;:cx00000kdxxxxxxkKKKx'.      
+                                               ......',:coooollcccccccc;,kKKKKKKk:;,;;:lxOO000Oddxxxxxx0KKKl'.....  
+                                          .....'',;:cooddddollcc::;,,,,';OKKKKKKKkooxO00OOkkkkxxxxxxxxOKKKkcc::;;,'.
+                                        ..',,,;::cclooolc:;,,,,,,,,'',;:d0KKKKKKKK0xddkOKKK0OxddxxxxxOKKKk:;;;;;;;'.
+                                        ..cddddoooodlc::;;,,,'',;:cldx0KKKKKKKKKKKK0OxxxxOOOdddxxxxk0KKKk:,,,,,,''..
+                                        ..';:cllcccc:;,,''....;okkkkk00KKKKKKKKKKKKKK0OkxddddxxxkkOKKKOl,''''...... 
+                                          ................  ...':okOO00000KKKK0KKKKKKKKKK00OOO00KKKKOo,...          
+                                                               ...':ok0KKKKKKK000KKKKKKKKKKKKKKKK0xc'..             
+                                                                  ....';:lodxkO00000KKKK00kxdolc;'...               
+                                                                      ........',,;;;;:;;;,'.......                  
+                                                                            ................."
+echo -e "\e[1;36m                                                                         ___                \e[1;32m_"
+echo -e "\e[1;36m                                                                        / __|\e[1;31m___ \e[1;33m ___ \e[1;36m __ _\e[1;32m| |\e[1;31m___ "
+echo -e "\e[1;36m                                                                       | (_ \e[1;31m/ _ \\\\\e[1;33m/ _ \\\\\e[1;36m/ _\` \e[1;32m|  \e[1;31m -_)"
+echo -e "\e[1;36m                                                                        \___\e[1;31m\___/\e[1;33m\___/\e[1;36m\__, \e[1;32m|_\e[1;31m\___|"
+echo -e "\e[1;37m                                                                            Developers\e[1;36m|___/\e[1;37mAracaju"
+echo -e ""
+echo -e ""
+echo -e "                                                                    \e[1;35mJefersson Nathan de Oliveira Chaves"
+echo -e "                                                                       \e[1;33m(${CURRENT_DATETIME})"
+read slide
--- slides/php.sh
+++ slides/php.sh
@@ -10,35 +10,35 @@ echo -e "
 
 
 
-                                           .',:cccccccccccccccccc:,'.                 
-                                     .;loolc:,..                ..,:cllll;.           
-                                 'ccc:,.            :oo:                .':ccc'       
-                              .ld:.                .XMMl                     .:ol.    
-                            'dl.     .loooooolc;.  :MMMkooolc,.  coooooolc;.    .co'  
-                           dx.       :MMWOOOOXMMWc kMMXOOO0WMM0 ;WMM0OOOXMMNl      dd 
-                          dx         kMMO     kMMX:NMMc    xMMK.kMM0.    kMMW'      dd
-                          0.        'NMMc     OMMOxMMX.   .KMMd,WMWc    .0MMK.      .0
-                          0.        dMMN'  .,xMMXlXMMd    cWMW,oMMK.  .,OMMK'       .0
-                          lO.      .XMMMNNNWWNOl.cMMX.   .0MMO.KMMWNNNWWXOl.       .kl
-                           :k.     lMMMo'''..    .''.     .''.;WMWc''...          .x: 
-                            .lo;   OMMK.                      dMM0              ,ol.  
-                               ;do:,''.                       .''.           ,oo;     
-                                 .,ccc:,.                              .,:ccc,.       
-                                      .,cllllc;,...          ...,;cllllc,.            
-                                             .',;::ccc::::c::::;,'.                   
+                                                                         .',:cccccccccccccccccc:,'.                 
+                                                                   .;loolc:,..                ..,:cllll;.           
+                                                               'ccc:,.            :oo:                .':ccc'       
+                                                            .ld:.                .XMMl                     .:ol.    
+                                                          'dl.     .loooooolc;.  :MMMkooolc,.  coooooolc;.    .co'  
+                                                         dx.       :MMWOOOOXMMWc kMMXOOO0WMM0 ;WMM0OOOXMMNl      dd 
+                                                        dx         kMMO     kMMX:NMMc    xMMK.kMM0.    kMMW'      dd
+                                                        0.        'NMMc     OMMOxMMX.   .KMMd,WMWc    .0MMK.      .0
+                                                        0.        dMMN'  .,xMMXlXMMd    cWMW,oMMK.  .,OMMK'       .0
+                                                        lO.      .XMMMNNNWWNOl.cMMX.   .0MMO.KMMWNNNWWXOl.       .kl
+                                                         :k.     lMMMo'''..    .''.     .''.;WMWc''...          .x: 
+                                                          .lo;   OMMK.                      dMM0              ,ol.  
+                                                             ;do:,''.                       .''.           ,oo;     
+                                                               .,ccc:,.                              .,:ccc,.       
+                                                                    .,cllllc;,...          ...,;cllllc,.            
+                                                                           .',;::ccc::::c::::;,'.                   
 
 
 
 
 
 "
-echo -e "\e[1;36m                                             ___                \e[1;32m_"
-echo -e "\e[1;36m                                            / __|\e[1;31m___ \e[1;33m ___ \e[1;36m __ _\e[1;32m| |\e[1;31m___ "
-echo -e "\e[1;36m                                           | (_ \e[1;31m/ _ \\\\\e[1;33m/ _ \\\\\e[1;36m/ _\` \e[1;32m|  \e[1;31m -_)"
-echo -e "\e[1;36m                                            \___\e[1;31m\___/\e[1;33m\___/\e[1;36m\__, \e[1;32m|_\e[1;31m\___|"
-echo -e "\e[1;37m                                                Developers\e[1;36m|___/\e[1;37mAracaju"
-echo -e "                 "
+echo -e "\e[1;36m                                                                         ___                \e[1;32m_"
+echo -e "\e[1;36m                                                                        / __|\e[1;31m___ \e[1;33m ___ \e[1;36m __ _\e[1;32m| |\e[1;31m___ "
+echo -e "\e[1;36m                                                                       | (_ \e[1;31m/ _ \\\\\e[1;33m/ _ \\\\\e[1;36m/ _\` \e[1;32m|  \e[1;31m -_)"
+echo -e "\e[1;36m                                                                        \___\e[1;31m\___/\e[1;33m\___/\e[1;36m\__, \e[1;32m|_\e[1;31m\___|"
+echo -e "\e[1;37m                                                                            Developers\e[1;36m|___/\e[1;37mAracaju"
 echo -e ""
-echo -e "                                        \e[1;35mJefersson Nathan de Oliveira Chaves"
-echo -e "                                           \e[1;33m(${CURRENT_DATETIME})"
+echo -e ""
+echo -e "                                                                    \e[1;35mJefersson Nathan de Oliveira Chaves"
+echo -e "                                                                       \e[1;33m(${CURRENT_DATETIME})"
 read slide
