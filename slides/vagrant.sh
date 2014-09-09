f58803be19f24c02494967b95ae9f7b353818c90
malukenho <admin@phpse.net>
2014-09-09 05:13:54 -0300

    Add vagrant logo slide for presentation

---
+++ slides/vagrant.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+clear
+echo -e "\e[1;34m
+\e[1;34m                                                   .cKNNNk,                                         ,c\e[0;34m:.    
+\e[1;34m                                                'cxXNNNNNNN0x:.                              .:x0Oo;:\e[0;34mlox0kc'
+\e[1;34m                                                OXNNNNNNNNNNNNN0o,                        ,dKNNNNNNNK\e[0;34mKNNNNXK
+\e[1;34m                                                cldOXNNNNNNNNNNNNNKx;                  ;xXNNNNNNNNNN\e[0;34mNNNK0kxx
+\e[1;34m                                                cccoxkKKKNNNNNNNNNNN\e[0;34mNKc\e[1;34m              :0NNNNNK0XNNN\e[0;34mNX0kocoxxx
+\e[1;34m                                                :ccoOXNxldkKNNNN\e[0;34mNXK0kxc\e[1;34m              ;cok0OxdllxOO\e[0;34mkxxxdodxxl
+\e[1;34m                                                .cccldkoccccox\e[0;34mO0Oxxxxxo\e[1;34m              :cccdxddlccl\e[0;34mxxxxxxxxxo.
+\e[1;34m                                                 .ccccccccccccl\e[0;34mdxxxdxxx;\e[1;34m            'cccloolccccl\e[0;34mxxxxxxxxd. 
+\e[1;34m                                                  .cccccccccccl\e[0;34mkKN0loodd'\e[1;34m          .ccccccccllccl\e[0;34mxxxxxxxx,  
+\e[1;34m                                                   ;ccccccccccl\e[0;34mdOK0lccoxd.\e[1;34m        .ccccccox0XX0k\e[0;34mdxxxxxxx:   
+\e[1;34m                                                    ,:cccccccccl\e[0;34moxxdoloxxl\e[1;34m        :ccccccldOKX0\e[0;34mOxxxxxxxl    
+\e[1;34m                                                      .cccccccccl\e[0;34mdxxxxxxxx;\e[1;34m      .ccccccccccld\e[0;34mxxxxxxxxl     
+\e[1;34m                                                      .ccccccccccl\e[0;34mdxxxxxxxd,\e[1;34m     .cccccccloll\e[0;34mdxxxxxxxd.     
+\e[1;34m                                                      .ccccccccccc\e[0;34modxxxxxxxx'\e[1;34m    'cccccccoxx\e[0;34mddxxxxxxx,      
+\e[1;34m                                                       'cccoolccccc\e[0;34moxxxxxxxxo.\e[1;34m.':cccccccldx\e[0;34mxxxdooxxd,       
+\e[1;34m                                                        'cldxO0Oxlcc\e[0;34mdxxxxxxxxl\e[1;34m:cccccccccld\e[0;34mxxxdlllxx;        
+\e[1;34m                                                         ;cd0NNNNOocl\e[0;34mdxxxxxxxd\e[1;34mcccccccccccd\e[0;34mxxxxxddxc         
+\e[1;34m                                                         .:ccoxxoccccl\e[0;34mdxxxxxxd\e[1;34mcccccccoxol\e[0;34mdxxxxxxxc          
+\e[1;34m                                                          .ccccccccccc\e[0;34mldxxxxxd\e[1;34mcccccclKMNk\e[0;34mdxxxxxxo.          
+\e[1;34m                                                           .ccccccccccc\e[0;34moxxxxxd\e[1;34mcccccckK\e[0;34mOkxxxxxxxo.           
+\e[1;34m                                                            .cccccccccc\e[0;34mloxxxxd\e[1;34mccccco\e[0;34mxxxxxxxxxxd.            
+\e[1;34m                                                             'ccccccccccl\e[0;34mdxxxx\e[1;34mocccld\e[0;34mxxxxxxxxxx,             
+\e[1;34m                                                              ;cccllcccccl\e[0;34mx0XN\e[1;34mNKOdd\e[0;34mxxxxxddxxxc              
+\e[1;34m                                                               :ccoddolcccl\e[0;34mox0\e[1;34mKOk\e[0;34mxxxxxolldxxl               
+\e[1;34m                                                                ;cloolccccccccllo\e[0;34mdxxx0KOddxl                
+\e[1;34m                                                                .cccccccccccclc\e[0;34mcclxxxOKOxxd.                
+\e[1;34m                                                                 .ccccccccox0\e[0;34m0lcclxxxxxxxx,                 
+\e[1;34m                                                                  .,ccccccokK0\e[0;34mlcclxxxxxo:'                  
+\e[1;34m                                                                     .':cccccl\e[0;34mdollxxl,.                     
+\e[1;34m                                                                        ..;ccl\e[0;34mxxdc,.                        
+\e[1;34m                                                                            .'\e[0;34m,.                            
+\e[1;34m                                                ...    ..  ...    ......  ......     ...   ...   ...........
+\e[1;34m                                                .c:   'c, .ccc.  .c:,,,' .c:,,;c;   .ccc.  .cc;  ;c.,,;cc,,.
+\e[1;34m                                                 ,c, .cc..cc'::  .c.     .c;  .:c.  ::'c;  .c:c:.;c.  .c;   
+\e[1;34m                                                  ;c.,c. ,c:';c; .c.  .c..cccccc'  ;c;':c' .c',c::c.  .c;   
+\e[1;34m                                                  .ccc, .c:,,,:c..c;..,c..c;..:c;..c:,,,:c..c. 'ccc.  .c;   
+                                                   ;cc. ;c.   .c;.cccccc..c,  .:c;;c.   'c;.c.  ,cc.  .c;  
+"
+read slide
