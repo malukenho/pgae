c9d1eb9931d0e13579fab82949b34824679c89ba
malukenho <admin@phpse.net>
2014-09-30 18:27:09 -0300

    Add download sdk slide

---
+++ slides/download_sdk.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+clear
+echo -e "
+\e[1;32m
+                                                                        ,;;;;;;;;;;;;;;;;;;;;;;;;.                          
+                                                                       :kO0000000000000000000000kx,                         
+                                                                       ck0OkkkkkkkkkkkkkkkkkkkkOOkc                         
+                                                                       ckkkkkkkkkkkkkkkkkkkkkkkkkkc                         
+                                                                       ckkkkkkkkkkkkkkkkkkkkkkkkkkc                         
+                                                                       ckkkkkkkkkkkkkkkkkkkkkkkkkkc                         
+                                                                       ckkkkkkkkkkkkkkkkkkkkkkkkkkc                         
+                                                                       ckkkkkkkkkkkkkkkkkkkkkkkkkkc                         
+                                                                       ckkkkkkkkkkkkkkkkkkkkkkkkkkc                         
+                  \e[1;33m   ____  _   _ ____    ____      _ _ \e[1;32m                ckkkkkkkkkkkkkkkkkkkkkkkkkkc
+                  \e[1;33m  |  _ \| | | |  _ \  / ___|  __| | | __ \e[1;32m            ckkkkkkkkkkkkkkkkkkkkkkkkkkc
+                  \e[1;33m  | |_) | |_| | |_) | \___ \ / _\` | |/ / \e[1;32m            ckkkkkkkkkkkkkkkkkkkkkkkkkkc
+                  \e[1;33m  |  __/|  _  |  __/   ___) | (_| |   <   \e[1;32m           cxxxxxxxxxxxxxxxxxxxxxxxxxxc                         
+                  \e[1;33m  |_|   |_| |_|_|     |____/ \__,_|_|\_\  \e[1;32m           cxxxxxxxxxxxxxxxxxxxxxxxxxxc
+                                                                       cxxxxxxxxxxxxxxxxxxxxxxxxxxc                         
+                                                                       cxxxxxxxxxxxxxxxxxxxxxxxxxxc                         
+                                                                       cxxxxxxxxxxxxxxxxxxxxxxxxxxc                         
+                                                            ...........cxxxxxxxxxxxxxxxxxxxxxxxxxxc..........               
+                                                          ,dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxl.             
+                                                          .oxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx:              
+                                                           .cdxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxdxo'               
+                                                             ;ddddddddddddddddddddddddddddddddddddddddddddc.                
+                                                              .lddddddddddddddddddddddddddddddddddddddddo,                  
+                                                                :odddddddddddddddddddddddddddddddddddddl.                   
+                                                                 .lddddddddddddddddddddddddddddddddddo;.                    
+                                                                  .cddddddddddddddddddddddddddddddddo'                      
+                                                                    .lddddddddddddddddddddddddddddo:.                       
+                                                                     .:ooooooooooooooooooooooooool.                         
+                                                                       'oooooooooooooooooooooooo:.                          
+                                                                        .cooooooooooooooooooooo,                            
+                                                                          ,loooooooooooooooool.                             
+                                                                           .cooooooooooooool,                               
+                                                                             ;ooooooooooool.                                
+                                                                              'looooooooo:.                                 
+                                                                               'looooooo:.                
+                                                                                 ddodd
+                                                                                  xxx\e[1;37m
+
+"
+read slide
