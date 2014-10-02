0cd392510400bee7b39bde28b5b728919fb2cc70
malukenho <admin@phpse.net>
2014-10-01 22:32:30 -0300

    Added slides for better presentation flow
    - makefile is now improved and organized by topics
    - DevFest Nordeste Intro slider :)
    - Step 2 Slider intro
    - Why PHP? Slider
    - Why Google App Engine? Slider
    
    Awesome 8)

--- makefile
+++ makefile
@@ -1,14 +1,31 @@
 presentation:
+	# Introduction
+	slides/gdg.sh
 	slides/app_engine.sh
-	slides/intro.sh
 	slides/php.sh
+
+	# self presentation
 	slides/whoami.sh
+
+	# why php
+	slides/why_php.sh
+
+	# why gae
+	slides/why_gae.sh
+	slides/cloud.sh
+	slides/pass.sh
+
+	# LAMP
 	slides/lamp.sh
 	slides/linux.sh
 	slides/apache.sh
 	slides/mysql.sh
 	slides/elephpant.sh
-	slides/cloud.sh
-	slides/pass.sh
+
+	# Step 1 - Instalation
 	slides/step1.sh
+	slides/download_sdk.sh
+
+	# Step 2 - Environment
+	slides/step2.sh
 	slides/vagrant.sh
---
+++ slides/gdg.sh
@@ -0,0 +1,46 @@
+#!/bin/zsh
+clear
+echo -e "
+\e[1;37m
+
+
+
+
+
+                                                              \e[0;34mllllllll\e[0;31mccccccccccc:,.                  
+                                                              \e[0;34m,lllllllll\e[0;31mccccccccccccc;                
+                                                               \e[0;34m.cllllllll\e[0;31mccccccccccccc:.              
+                                                                \e[0;34m.ccllccllll\e[0;31mcccccccccccc:.             
+                                                                  \e[0;34m;clllllllll\e[0;31mcccccccccccc.            
+                                                                   \e[0;34m;llllllllll\e[0;31mcccccccccccc,           
+                                                                    \e[0;34m'mllllllllll\e[0;31mcccccccccccc;          
+                                                                     \e[0;34m.cllllllllll\e[0;31mccccccccccc;.        
+                                                                      \e[0;34m.cllllllllll\e[0;31mccccccccccc:.       
+                                                                       \e[0;34m.:lllllllllll\e[0;31mccccccccccc'      
+                                                                         \e[0;34m;lllllllllll\e[0;31mccccccccccc,     
+                                                                          \e[0;34m.lllllllllll\e[0;31mccccccccccc;.   
+                                                                           \e[0;34m.llllllllllll\e[0;31mcccccccccc:.  
+                                                                             \e[0;34mclllllllllll\e[0;31mcccccccccc:. 
+                            \e[1;37m ____             _____         _                 \e[0;34m:ollllllllll\e[0;31mcccccccccc:'
+                            \e[1;37m|  _ \  _____   _|  ___|__  ___| |_               \e[0;34m:oolllllllll\e[0;31mcccccccccc:
+                            \e[1;37m| | | |/ _ \ \ / / |_ / _ \/ __| __|                \e[0;34m'oooollllllll\e[0;31mcccccccc::
+                            \e[1;37m| |_| |  __/\ V /|  _|  __/\__ \ |_                 \e[0;32m:ol\e[0;34mlllllllllll\e[0;31mccccccc::
+                            \e[1;37m|____/ \___| \_/ |_|  \___||___/\__|              \e[0;32m:ooll\e[0;34mlllllllllll\e[0;31mcccccc:'                                               
+                          \e[1;37m _   _               _           _                 \e[0;32m.coollll\e[0;34mlllllllllll\e[0;31mccc::. 
+                          \e[1;37m| \ | | ___  _ __ __| | ___  ___| |_ ___           \e[0;32m'oooollllcc\e[0;34mlllllllllll\e[0;31mc:.
+                          \e[1;37m|  \| |/ _ \| '__/ _\` |/ _ \/ __| __/ _ \        \e[0;32m'cllolllllcl\e[1;33mkx\e[0;34mlllllllll:.   
+                          \e[1;37m| |\  | (_) | | | (_| |  __/\__ \ ||  __/      \e[0;32m':::cccllllo\e[1;33mOOOx\e[0;34mollllll;     
+                          \e[1;37m|_| \_|\___/|_|  \__,_|\___||___/\__\___|      \e[0;32m;::::::::ccx\e[1;33mOOOOkk\e[0;34mollll'      
+                                                                      \e[0;32m.;:::::::::c\e[1;33mk00OOOkkk\e[0;34mllc.       
+                                                                     \e[0;32m.::::::::::\e[1;33moO00000OOkko\e[0;34m:.        
+                                                                    \e[0;32m'::::::::::\e[1;33md0000000000Oc          
+                                                                   \e[0;32m,::::::::::\e[1;33mx00000000000l.          
+                                                                  \e[0;32m;:::::::::\e[1;33mcx00000000000c            
+                                                                \e[0;32m.;:::::::::\e[1;33mck0000000000O;             
+                                                              \e[0;32m.;:::::::::\e[1;33moO0000000000o.               
+                                                              \e[0;32m;;;;;;;;;:\e[1;33md0000000000Oc                 
+                                                              \e[0;32m;;;;;;;;;\e[1;33mo00000000d. 
+
+
+"
+read slide
