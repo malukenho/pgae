4111fb73f6ea37fcebf9c5c690a790626d5a1cc2
malukenho <admin@phpse.net>
2014-10-09 02:47:27 -0300

    Create slide for Register Application action

---
+++ slides/register_app.sh
@@ -0,0 +1,44 @@
+#!/bin/zsh
+clear
+echo -e "
+\e[1;37m
+
+
+
+
+
+
+
+                                                                                ,,
+                                                \`7MM\"\"\"Mq.                      db            mm
+                                                  MM   \`MM.                                   MM
+                                                  MM   ,M9  .gP\"Ya   .P\"Ybmmm \`7MM  ,pP\"Ybd mmMMmm .gP\"Ya \`7Mb,od8
+                                                  MMmmdM9  ,M'   Yb :MI  I8     MM  8I   \`\"   MM  ,M'   Yb  MM' \"'
+                                                  MM  YM.  8M\"\"\"\"\"\"  WmmmP\"     MM  \`YMMMa.   MM  8M\"\"\"\"\"\"  MM
+                                                  MM   \`Mb.YM.    , 8M          MM  L.   I8   MM  YM.    ,  MM
+                                                .JMML. .JMM.\`Mbmmd'  YMMMMMb  .JMML.M9mmmP'   \`Mbmo\`Mbmmd'.JMML.
+                                                                    6'     dP
+                                                                    Ybmmmd'
+
+                                                                      ,,    ,,                          ,,
+                                          db                        \`7MM    db                   mm     db
+                                         ;MM:                         MM                         MM
+                                        ,V^MM.   \`7MMpdMAo.\`7MMpdMAo. MM  \`7MM  ,p6\"bo   ,6\"Yb.mmMMmm \`7MM  ,pW\"Wq.\`7MMpMMMb.
+                                       ,M  \`MM     MM   \`Wb  MM   \`Wb MM    MM 6M'  OO  8)   MM  MM     MM 6W'   \`Wb MM    MM
+                                       AbmmmqMA    MM    M8  MM    M8 MM    MM 8M        ,pm9MM  MM     MM 8M     M8 MM    MM
+                                      A'     VML   MM   ,AP  MM   ,AP MM    MM YM.    , 8M   MM  MM     MM YA.   ,A9 MM    MM
+                                    .AMA.   .AMMA. MMbmmd'   MMbmmd'.JMML..JMML.YMbmd'  \`Moo9^Yo.\`Mbmo.JMML.\`Ybmd9'.JMML  JMML.
+                                                   MM        MM
+                                                 .JMML.    .JMML.
+
+
+
+
+
+
+
+
+
+
+"
+read slide
